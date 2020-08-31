65e16f91ee35a219b530a768557bc3f3d4edfa33
paulmayer-tsp <paul.mayer@telecom-sudparis.eu>
2020-08-31 05:18:17 +0200

    Updates

---
+++ precache-manifest.62b35b3f70ad00b7393e51e0d48aa880.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebf398410585549499a054b51e74e4f7",
+    "url": "/rdf-fusion/index.html"
+  },
+  {
+    "revision": "6b19caa1b738abc0cc35",
+    "url": "/rdf-fusion/static/css/main.5969d45b.chunk.css"
+  },
+  {
+    "revision": "f12f218ab0e7a70e1b59",
+    "url": "/rdf-fusion/static/js/2.00cf6e55.chunk.js"
+  },
+  {
+    "revision": "73a2b7240a69473034d762e07d0cac38",
+    "url": "/rdf-fusion/static/js/2.00cf6e55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b19caa1b738abc0cc35",
+    "url": "/rdf-fusion/static/js/main.294e7a4c.chunk.js"
+  },
+  {
+    "revision": "207873385e1804426bb7",
+    "url": "/rdf-fusion/static/js/runtime-main.5d357ce8.js"
+  },
+  {
+    "revision": "261701509ea3c105df62ad9d91798507",
+    "url": "/rdf-fusion/static/media/fraudbuster_logo.26170150.svg"
+  },
+  {
+    "revision": "48316ef4e690c3a65e0b2d0b5e5524a2",
+    "url": "/rdf-fusion/static/media/icon_category.48316ef4.svg"
+  },
+  {
+    "revision": "cb441765ed012ef3eec278619a38effe",
+    "url": "/rdf-fusion/static/media/icon_eye.cb441765.svg"
+  },
+  {
+    "revision": "c616cb6fb917dd70627f3c3d7124275a",
+    "url": "/rdf-fusion/static/media/icon_key.c616cb6f.svg"
+  },
+  {
+    "revision": "11526d759de7b8e6eea3850a92e22420",
+    "url": "/rdf-fusion/static/media/icon_leaf.11526d75.svg"
+  },
+  {
+    "revision": "0e3ddf7c0714c7f7bbb2fd6f78646c51",
+    "url": "/rdf-fusion/static/media/icon_lock.0e3ddf7c.svg"
+  },
+  {
+    "revision": "f93544c03400b4e72743d76468f5c6b4",
+    "url": "/rdf-fusion/static/media/icon_message.f93544c0.svg"
+  },
+  {
+    "revision": "fdc4a3387f9ae168bd94d97391f64c78",
+    "url": "/rdf-fusion/static/media/icon_person.fdc4a338.svg"
+  }
+]);
